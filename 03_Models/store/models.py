279f782cfa204d8f79cc43173b8feddf1690c586
0x1Shub <shubhamyeram13@gmail.com>
2025-03-27 15:07:09 +0530

    One to Many Relationship

--- 03_Models/store/models.py
+++ 03_Models/store/models.py
@@ -39,10 +39,13 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add = True)
     payement_status = models.CharField(max_length=1, choices=PAYMENT_CHOICES, default=PAYMENT_PENDING)
 
+    # One-to-Many Relationship
+    customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
+
 
 # One-to-One relationship
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
-    
+
