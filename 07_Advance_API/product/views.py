071fd4c57a4654c1d23781d9211e26d5877f0864
0x1Shub <shubhamyeram13@gmail.com>
2025-04-16 11:23:33 +0530

    Class Based Views

---
+++ 07_Advance_API/product/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework.views import APIViews
+from rest_framework.http import Response
+from .models import Product
+
+# Create your views here.
+# Class Based Views
+class ProductList(APIViews):
+    def get(self, request):
+        queryset = Product.objects.select_related('collection').all
+        serializer = ProductSerializer(
+            queryset, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def post(self, response):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
